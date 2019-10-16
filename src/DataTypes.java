2928ae3b99fa43366b3245b696841edda006a035
Gilbert <kevin.gilbert@accenture.com>
2019-10-16 13:17:27 -0400

    Implemented calculation functionality and added test cases

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -5,26 +5,23 @@ import java.util.Stack;
 //and implementing a stack to store our calculations
 public class DataTypes {
     public static void main(String[] args) {
-
-        Stack stack = populateStack("2x^3+3x^2+4x+5", 2);
-
-        while(!stack.isEmpty()){
-            System.out.println(stack.pop());
-        }
+        Stack<String> stack = populateStack("2x^3+3x^2+4x+5", 2);
+        System.out.println(stack);
+        calculate(stack);
     }
 
-    public static Stack populateStack(String equation, int xValue) {
-        Stack stack = new Stack();
+    public static Stack<String> populateStack(String equation, int xValue) {
+        Stack<String> stack = new Stack<>();
         ArrayList<String> operators = new ArrayList<>();
 
         for (int i = 0; i < equation.length(); i++) {
             //If the character equals x replace with the value of x and add a multiply sign
             if (Character.toString(equation.charAt(i)).equals("x")) {
-                stack.push(xValue);
+                stack.push(xValue + "");
                 operators.add("*");
                 //Adding the numbers to the stack
             } else if (Character.isDigit(equation.charAt(i))) {
-                stack.push(equation.charAt(i));
+                stack.push(equation.charAt(i) + "");
                 //If character equals power, divide or multiply add it to the operators array
             } else if (Character.toString(equation.charAt(i)).equals("^") || Character.toString(equation.charAt(i)).equals("/") || Character.toString(equation.charAt(i)).equals("*")) {
                 operators.add(Character.toString(equation.charAt(i)));
@@ -35,7 +32,13 @@ public class DataTypes {
                     stack.push(operators.get(j));
                 }
                 operators.clear();
-                stack.push(equation.charAt(i));
+                operators.add(Character.toString(equation.charAt(i)));
+            }
+        }
+        //Making sure we have all the operators added to the stack
+        for (String operator : operators) {
+            if (operator != null) {
+                stack.push(operator);
             }
         }
         return stack;
@@ -44,25 +47,47 @@ public class DataTypes {
     public static ArrayList<String> orderOperators(ArrayList<String> operators) {
         ArrayList<String> sortedOperators = new ArrayList<>();
 
-        if(operators.contains("^")) sortedOperators.add("^");
-        if(operators.contains("*")) sortedOperators.add("*");
-        if(operators.contains("/")) sortedOperators.add("/");
-        if(operators.contains("+")) sortedOperators.add("+");
-        if(operators.contains("-")) sortedOperators.add("-");
+        if (operators.contains("^")) sortedOperators.add("^");
+        if (operators.contains("*")) sortedOperators.add("*");
+        if (operators.contains("/")) sortedOperators.add("/");
+        if (operators.contains("+")) sortedOperators.add("+");
+        if (operators.contains("-")) sortedOperators.add("-");
 
         return sortedOperators;
     }
 
-    public static int calculate(Stack stack){
-        int result = 0;
-        ArrayList numArray = new ArrayList();
-        Object temp = stack.pop();
+    public static Stack<String> calculate(Stack<String> stack) {
+        Stack<String> equationStack = new Stack<>();
+        Stack<String> reversedStack = new Stack<>();
 
-        if(Character.isDigit((Character) temp)){
-            numArray.add(temp);
-        } else if(Character.toString((Character) stack.pop()).equals("^")){
-            //Math.pow((int)numArray.get(numArray.lastIndexOf(numArray), ))
+        while (!stack.isEmpty()) {
+            reversedStack.push(stack.pop());
         }
-        return result;
+
+        while (!reversedStack.isEmpty()) {
+            if (reversedStack.peek().equals("^")) {
+                reversedStack.pop();
+                double base = Integer.parseInt(equationStack.pop() + "");
+                double power = Integer.parseInt(equationStack.pop() + "");
+                equationStack.push((int)Math.pow(power, base) + "");
+            } else if (reversedStack.peek().equals("*")) {
+                reversedStack.pop();
+                equationStack.push(Integer.parseInt(equationStack.pop()) * Integer.parseInt(equationStack.pop()) + "");
+            } else if (reversedStack.peek().equals("/")) {
+                reversedStack.pop();
+                equationStack.push(Integer.parseInt(equationStack.pop()) / Integer.parseInt(equationStack.pop()) + "");
+            } else if (reversedStack.peek().equals("+")) {
+                reversedStack.pop();
+                equationStack.push(Integer.parseInt(equationStack.pop()) + Integer.parseInt(equationStack.pop()) + "");
+            } else if (reversedStack.peek().equals("-")) {
+                reversedStack.pop();
+                equationStack.push(Integer.parseInt(equationStack.pop()) - Integer.parseInt(equationStack.pop()) + "");
+            } else {
+                equationStack.push(reversedStack.pop());
+            }
+            //Showing how the equations is being executed
+            System.out.println(equationStack);
+        }
+        return equationStack;
     }
 }
--- src/DataTypesTest.java
+++ src/DataTypesTest.java
@@ -1,14 +1,89 @@
+import java.util.Stack;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DataTypesTest {
-    //TODO: add test cases when calculation method is complete
     @org.junit.jupiter.api.Test
     void populateStack() {
-    }
+        //Test equation 1
+        Stack<String> stack1 = DataTypes.populateStack("2x^3+3x^2+4x+5", 2);
+        Stack<String> stack1Expected = new Stack<>();
+        stack1Expected.push("2");
+        stack1Expected.push("2");
+        stack1Expected.push("3");
+        stack1Expected.push("^");
+        stack1Expected.push("*");
+        stack1Expected.push("3");
+        stack1Expected.push("2");
+        stack1Expected.push("2");
+        stack1Expected.push("^");
+        stack1Expected.push("*");
+        stack1Expected.push("+");
+        stack1Expected.push("4");
+        stack1Expected.push("2");
+        stack1Expected.push("*");
+        stack1Expected.push("+");
+        stack1Expected.push("5");
+        stack1Expected.push("+");
+        assertEquals(stack1Expected, stack1);
 
-    @org.junit.jupiter.api.Test
-    void orderOperators() {
+        //Test equation 2
+        Stack<String> stack2 = DataTypes.populateStack("2x^3+4x+1", 3);
+        Stack<String> stack2Expected = new Stack<>();
+        stack2Expected.push("2");
+        stack2Expected.push("3");
+        stack2Expected.push("3");
+        stack2Expected.push("^");
+        stack2Expected.push("*");
+        stack2Expected.push("4");
+        stack2Expected.push("3");
+        stack2Expected.push("*");
+        stack2Expected.push("+");
+        stack2Expected.push("1");
+        stack2Expected.push("+");
+        assertEquals(stack2Expected, stack2);
+
+        //Test equation 3
+        Stack<String> stack3 = DataTypes.populateStack("4x^3+x^2+4x+8", 5);
+        Stack<String> stack3Expected = new Stack<>();
+        stack3Expected.push("4");
+        stack3Expected.push("5");
+        stack3Expected.push("3");
+        stack3Expected.push("^");
+        stack3Expected.push("*");
+        stack3Expected.push("5");
+        stack3Expected.push("2");
+        stack3Expected.push("^");
+        stack3Expected.push("+");
+        stack3Expected.push("4");
+        stack3Expected.push("5");
+        stack3Expected.push("*");
+        stack3Expected.push("+");
+        stack3Expected.push("8");
+        stack3Expected.push("+");
+        assertEquals(stack3Expected, stack3);
+        //Failed because I am inserting a multiply sign after hitting a x variable
     }
 
     @org.junit.jupiter.api.Test
     void calculate() {
+        //Test calculation 1
+        Stack<String> stack1 = DataTypes.populateStack("2x^3+3x^2+4x+5", 2);
+        Stack<String> stack1Expected = new Stack<>();
+        stack1Expected.push("41");
+        assertEquals(stack1Expected, DataTypes.calculate(stack1));
+
+        //Test calculation 1
+        Stack<String> stack2 = DataTypes.populateStack("2x^3+4x+1", 3);
+        Stack<String> stack2Expected = new Stack<>();
+        stack2Expected.push("67");
+        assertEquals(stack2Expected, DataTypes.calculate(stack2));
+
+        //Test calculation 1
+        Stack<String> stack3 = DataTypes.populateStack("4x^3+x^2+4x+8", 5);
+        Stack<String> stack3Expected = new Stack<>();
+        stack3Expected.push("553");
+        assertEquals(stack3Expected, DataTypes.calculate(stack3));
+        //Failed because the stack isn't properly populated
     }
 }
